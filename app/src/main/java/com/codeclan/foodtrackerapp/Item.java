602f688f952a4df410a7c662bdb71ead5e433c26
Alice Loudon <alice_loudon@icloud.com>
2017-03-19 20:37:44 +0000

    Create new Android project and create Item class

---
+++ app/src/main/java/com/codeclan/foodtrackerapp/Item.java
@@ -0,0 +1,42 @@
+package com.codeclan.foodtrackerapp;
+
+/**
+ * Created by user on 19/03/2017.
+ */
+
+public class Item {
+
+    private String food;
+    private String meal;
+    private String date;
+
+    public Item(String food, String meal, String date) {
+        this.food = food;
+        this.meal = meal;
+        this.date = date;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getMeal() {
+        return meal;
+    }
+
+    public void setMeal(String meal) {
+        this.meal = meal;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
